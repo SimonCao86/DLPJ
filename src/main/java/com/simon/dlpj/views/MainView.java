33252feda4ac226123eff2d5d14d37921b26cd4b
SimonCao86 <cy_java@163.com>
2022-08-25 23:38:30 +0800

    初始版本

--- src/main/java/com/simon/dlpj/views/MainView.java
+++ src/main/java/com/simon/dlpj/views/MainView.java
@@ -2,13 +2,14 @@ package com.simon.dlpj.views;
 
 import de.felixroske.jfxsupport.AbstractFxmlView;
 import de.felixroske.jfxsupport.FXMLView;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
 /**
  * Created by caoyang on 2022-08-24
  **/
-@FXMLView(value = "/fxml/Main.fxml")
+@FXMLView(value = "/fxml/Main.fxml" )
 public class MainView extends AbstractFxmlView {
-
 
 
 }
